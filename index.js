311d6887624b5965be71ac52ab9d0dc6577203b0
luke francis <lukedevfrancis@gmail.com>
2023-05-04 14:33:15 -0400

    worked with objects- dot and bracket notation

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let name = "Luke"; 
+/*let name = "Luke"; 
 let age = 30; 
+*/
 
 let person = {
     name: "Luke",
     age: "30"
 };
+
+// dot notation
+person.name = "Joe";
+
+// bracket notation
+person['name'] = "Mary";
 
 console.log(person.name)
