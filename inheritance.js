69987fdf32e226257a3e9a8f6a0202f9ee46427c
Luke Francis <lukedevfrancis@gmail.com>
2023-06-05 22:35:08 -0400

    added code

--- inheritance.js
+++ inheritance.js
@@ -1,4 +1,8 @@
-import Teacher from './teacher';
+import Teacher, { promote } from './teacher';
+import React, { Component } from 'react'; // what you'll see in react
+
+// Default Exports -> import ... from '';
+// Named Exports -> import { ... } from '';
 
 const teacher = new Teacher('Luke', 'MSc');
 teacher.teach();
