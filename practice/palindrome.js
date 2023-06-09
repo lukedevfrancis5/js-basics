c3339403dc3f76e2d4090d2edf6762a00f8c8c5f
Luke Francis <lukedevfrancis@gmail.com>
2023-06-08 21:09:26 -0400

    added code

---
+++ practice/palindrome.js
@@ -0,0 +1,20 @@
+    function isPalindrome(str) {
+        // Remove spaces and punctuation, and convert to lowercase
+        str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+        // Compare characters from the beginning and end of the string
+        let i = 0;
+        let j = str.length - 1;
+
+        while (i < j) {
+            if (str[i] !== str[j]) {
+              return false; // Characters don't match, not a palindrome
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    console.log(isPalindrome('Racecar')) // output: true 
+    console.log(isPalindrome('hello')) // output: false 
