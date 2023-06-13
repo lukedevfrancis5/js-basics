1c0755a817d3db92b3618d9d76772e69567123a3
Luke Francis <lukedevfrancis@gmail.com>
2023-06-12 23:10:19 -0400

    worked on code

---
+++ practice/findMax.js
@@ -0,0 +1,11 @@
+
+function findMax(num) {
+    maxNum = 0;
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] > maxNum) {
+            maxNum = num[i];
+        }
+    }
+}
+
+console.log(findMax([5, 2, 9, 1, 7]));
